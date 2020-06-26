fa560ff899da6e56c9fc6e77ef7412efabd82d47
Gabriel Fernando Dyck <gabriel.dyck@despegar.com>
2020-06-26 01:00:30 -0300

    detalles al ejemplo de cond

--- gos-concurrency-building-blocks/the-sync-package/cond/fig-cond-based-queue.go
+++ gos-concurrency-building-blocks/the-sync-package/cond/fig-cond-based-queue.go
@@ -7,10 +7,12 @@ import (
 )
 
 func main() {
+	// Se inicializa cond
 	c := sync.NewCond(&sync.Mutex{})    // <1>
 	queue := make([]interface{}, 0, 10) // <2>
 
 	removeFromQueue := func(delay time.Duration) {
+		//Se agrega un delay para llenar la queue a proposito
 		time.Sleep(delay)
 		c.L.Lock()        // <8>
 		queue = queue[1:] // <9>
@@ -19,6 +21,10 @@ func main() {
 		c.Signal()   // <11>
 	}
 
+	//Lockeo Appendeo hasta que la cola tiene longitud 2.
+	//Mientras eso no ocurro itero haciendo lock y unlock para no tener condiciones de carrera con removeFromQueue
+	//Que comparten la queue
+	// Cuando la queue tiene longitud 2, se suspende el thread y se espera hasta que alguien le de signal
 	for i := 0; i < 10; i++ {
 		c.L.Lock()            // <3>
 		for len(queue) == 2 { // <4>

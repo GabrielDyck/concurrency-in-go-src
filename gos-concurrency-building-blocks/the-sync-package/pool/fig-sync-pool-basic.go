536e5efe6b084efdae43d659f79e98440890306b
Gabriel Fernando Dyck <gabriel.dyck@despegar.com>
2020-06-26 01:51:09 -0300

    detalles de pool

--- gos-concurrency-building-blocks/the-sync-package/pool/fig-sync-pool-basic.go
+++ gos-concurrency-building-blocks/the-sync-package/pool/fig-sync-pool-basic.go
@@ -13,8 +13,12 @@ func main() {
 		},
 	}
 
+	// Crea una instancia
 	myPool.Get()             // <1>
+	// Crea una instancia
 	instance := myPool.Get() // <1>
+	// Volves a dejar disponible la instancia
 	myPool.Put(instance)     // <2>
+	// Utilizas la que dejaste disponible
 	myPool.Get()             // <3>
 }
